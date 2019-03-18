eeb0d442a39b75bad4c559b3f9c727a80e30a225
RAMACHANDRUNI, THIRUMAL <thirumal.ramachandruni@cbp.dhs.gov>
2019-03-18 13:33:24 -0400

    fix

--- src/org/foo/Bar.groovy
+++ src/org/foo/Bar.groovy
@@ -1,5 +1,8 @@
-Class LogUtil {
-    log(String message){
+#!/usr/bin/env groovy
+package foo.bar 
+
+class LogUtil {
+    static void log(String message){
         println "I am from LogUtil!!!"
     }
 }
